d98356f8b1e08788ef80383550f08d581346562b
Arame-Z <zohrabyan_arame01@student.smc.edu>
2025-11-08 13:38:03 -0800

    trying night mode again

--- content.js
+++ content.js
@@ -3,21 +3,16 @@ console.log("Content script active");
 
 function applyNightMode(night) {
   if (night) {
-    // Dark mode using invert (works on all websites)
-    document.documentElement.style.filter = "invert(1) hue-rotate(180deg)";
+    document.documentElement.classList.add("night-mode");
   } else {
-    document.documentElement.style.filter = "";
+    document.documentElement.classList.remove("night-mode");
   }
 }
 
-// Load on page open
 chrome.storage.sync.get("night", ({ night }) => {
   applyNightMode(night);
 });
 
-// Apply whenever it changes
 chrome.storage.onChanged.addListener((changes) => {
-  if (changes.night) {
-    applyNightMode(changes.night.newValue);
-  }
+  if (changes.night) applyNightMode(changes.night.newValue);
 });
